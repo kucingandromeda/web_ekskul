bdb310f8e323a127d80802278308c60f617d33a1
kucingandromeda <agvayazem@gmail.com>
2024-02-15 16:57:50 +0700

    sudah membuat temapt photo ustad ego dan titlenya

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Head } from "./compunent/a_head/head";
+import { Ego } from "./compunent/b_ego/ego";
 
 function App() {
   return (
     <>
       <Head></Head>
+      <Ego></Ego>
     </>
   );
 }
---
+++ src/compunent/b_ego/ego.jsx
@@ -0,0 +1,21 @@
+export const Ego = () => {
+  return (
+    <div className="ego-section">
+      <h2>Pembimbing</h2>
+      <div className="ego-section-photo">
+        {/* <div className="ego-section-photo-bg"></div> */}
+        <div className="ego-photo"></div>
+      </div>
+      <div className="ego-name">
+        <h3>Ego Perdana Putra</h3>
+        <h3>Guru Informatika</h3>
+      </div>
+      <div className="ego-contact">
+        <div className="ego-contact-item"></div>
+        <div className="ego-contact-item"></div>
+        <div className="ego-contact-item"></div>
+        <div className="ego-contact-item"></div>
+      </div>
+    </div>
+  );
+};
